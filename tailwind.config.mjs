9461db6248d535fbb851bc044a3d9b8a8c689cc0
Jose Martinez <joariel17@gmail.com>
2024-11-25 11:55:22 -0300

    add animation dark, fix btn gmail

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,19 +4,6 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
-      // colors: {
-      //   primary1: '#001219',
-      //   primary2: '#005f73',
-      //   primary3: '#0a9396',
-      //   primary4: '#94d2bd',
-      //   primary5: '#e9d8a6',
-      //   secondary1: '#ee9b00',
-      //   secondary2: '#ca6702',
-      //   secondary3: '#bb3e03',
-      //   secondary4: '#ae2012',
-      //   secondary5: '#9b2226',
-      //   secondary6: '#000000',
-      // },
       animation: {
         'text-gradient': 'text-gradient 3s linear infinite',
       },
