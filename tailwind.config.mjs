76ea7db728a64f061f22a7e096341ab61debb667
Jose Martinez <joariel17@gmail.com>
2025-01-24 21:39:21 -0300

    fix nav & color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      animation: {
-        'text-gradient': 'text-gradient 3s linear infinite',
-      },
-      keyframes: {
-        'text-gradient': {
-          to: {
-            backgroundPosition: '200% center',
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
